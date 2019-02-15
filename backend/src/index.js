885bf7d9edf8b275589af64256d739821de03115
Andre Tonelli <aveigas@br.ibm.com>
2019-02-15 04:45:38 -0200

    Web socket configured - Backend Finished

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express ();
+
+const server = require('http').Server(app);
+
+const io = require('socket.io') (server);
 
 mongoose.connect(
     "mongodb+srv://omni-user:admin182@cluster0-orohr.mongodb.net/test?retryWrites=true",
@@ -11,10 +16,17 @@ mongoose.connect(
     }
 );
 
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000,  () => {
+server.listen(3000,  () => {
     console.log('app started on port 3000');
 });
 
